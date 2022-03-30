7e9a437caaeba8494b55bfc8486f6976926391e0
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-30 17:44:59 -0300

    req 4 - available dishes

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -18,12 +18,32 @@ class InventoryControl:
     def __init__(self):
         self.orders = []
         self.inventory = self.MINIMUM_INVENTORY.copy()
+        self.avaible_dishes = set(
+            ingredient for ingredient in self.INGREDIENTS.keys()
+        )
+        self.dishes_by_ingredient = self.get_dishes_by_ingredient()
+
+    def get_dishes_by_ingredient(self):
+        dishes_by_ing_result = {}
+        for ing in self.MINIMUM_INVENTORY.keys():
+            dishes_by_ing_result[ing] = set(
+                dish
+                for dish, dish_ings in self.INGREDIENTS.items()
+                if ing in dish_ings
+            )
+        return dishes_by_ing_result
 
     def update_inventory(self, order):
         for ingredient in self.INGREDIENTS[order]:
             self.inventory[ingredient] -= 1
+            if self.inventory[ingredient] < 1:
+                self.avaible_dishes.difference_update(
+                    self.dishes_by_ingredient[ingredient]
+                )
 
     def add_new_order(self, customer, order, day):
+        if order not in self.avaible_dishes:
+            return False
         self.orders.append({"customer": customer, "order": order, "day": day})
         self.update_inventory(order)
 
@@ -32,3 +52,6 @@ class InventoryControl:
         for item, value in self.MINIMUM_INVENTORY.items():
             buy_list[item] = value - self.inventory[item]
         return buy_list
+
+    def get_available_dishes(self):
+        return self.avaible_dishes
