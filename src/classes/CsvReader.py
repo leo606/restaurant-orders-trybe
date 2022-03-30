33f6008bd5872157db9669052655f4425dd7c848
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-29 21:55:25 -0300

    csv reader class

---
+++ src/classes/CsvReader.py
@@ -0,0 +1,23 @@
+import csv
+
+
+class CsvReader():
+    CSV_EXTENSION = "csv"
+
+    def get_file_extension(file_name):
+        return file_name.split(".")[-1].lower()
+
+    @classmethod
+    def read_file(cls, file_name):
+        file_extension = cls.get_file_extension(file_name)
+        if file_extension != cls.CSV_EXTENSION:
+            raise ValueError("Arquivo inválido")
+
+        with open(file_name) as file:
+            file_data = csv.DictReader(
+                file,
+                delimiter=",",
+                quotechar='"',
+                fieldnames=['name', 'dish', 'day']
+            )
+            return list(file_data)
