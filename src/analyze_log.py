6db795db053271c378d4f581210132bfceb226cf
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-30 08:37:31 -0300

    dishes never ordered days never went

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -17,6 +17,30 @@ def most_ordered(data, person):
     return (result, dishes_ordered)
 
 
+def dishes_never_ordered(data, person):
+    all_dishes = set()
+    person_dishes = set()
+
+    for order in data:
+        all_dishes.add(order['dish'])
+        if order['name'] == person:
+            person_dishes.add(order['dish'])
+
+    return all_dishes.difference(person_dishes)
+
+
+def days_never_went(data, person):
+    all_days = set()
+    person_went_days = set()
+
+    for order in data:
+        all_days.add(order['day'])
+        if order['name'] == person:
+            person_went_days.add(order['day'])
+
+    return all_days.difference(person_went_days)
+
+
 def analyze_log(path_to_file):
     restaurant_data = CsvReader.read_file(path_to_file)
     maria_info = most_ordered(restaurant_data, 'maria')
@@ -24,6 +48,8 @@ def analyze_log(path_to_file):
 
     print(maria_info[0])
     print(arnaldo_info[1]['hamburguer'])
+    print(dishes_never_ordered(restaurant_data, 'joao'))
+    print(days_never_went(restaurant_data, 'joao'))
 
 
 path = 'data/orders_1.csv'
--- src/classes/CsvReader.py
+++ src/classes/CsvReader.py
@@ -11,7 +11,7 @@ class CsvReader():
     def read_file(cls, file_name):
         file_extension = cls.get_file_extension(file_name)
         if file_extension != cls.CSV_EXTENSION:
-            raise ValueError("Arquivo inválido")
+            raise ValueError(f"Extensão inválida: '{file_name}'")
 
         with open(file_name) as file:
             file_data = csv.DictReader(
