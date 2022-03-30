3176fba8e63c5dfb2ae7ec14f4a6a5121b90930e
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-30 09:08:29 -0300

    csv reader error handling

--- src/classes/CsvReader.py
+++ src/classes/CsvReader.py
@@ -11,13 +11,16 @@ class CsvReader():
     def read_file(cls, file_name):
         file_extension = cls.get_file_extension(file_name)
         if file_extension != cls.CSV_EXTENSION:
-            raise ValueError(f"Extensão inválida: '{file_name}'")
+            raise FileNotFoundError(f"Extensão inválida: '{file_name}'")
 
-        with open(file_name) as file:
-            file_data = csv.DictReader(
-                file,
-                delimiter=",",
-                quotechar='"',
-                fieldnames=['name', 'dish', 'day']
-            )
-            return list(file_data)
+        try:
+            with open(file_name) as file:
+                file_data = csv.DictReader(
+                    file,
+                    delimiter=",",
+                    quotechar='"',
+                    fieldnames=['name', 'dish', 'day']
+                )
+                return list(file_data)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Arquivo inexistente: '{file_name}'")
